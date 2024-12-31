06da94f30eeb2cd664f6c942623e0a6804019732
SIGIRACE <chickang9@gmail.com>
2024-12-31 15:05:41 +0900

    feat: add Todo

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 import { ITodo } from "../interface";
 import Todo from "./ToDo";
+import { useEffect } from "react";
+import { todoSelector } from "../atom";
+import { useRecoilState } from "recoil";
+import ToDoInput from "./ToDoInput";
 
 const Container = styled.div`
   display: flex;
@@ -12,18 +16,25 @@ const Container = styled.div`
   margin: 0 auto;
 `;
 
-const ToDoItems: ITodo[] = [
-  { id: 1, title: "할 일 1할 일 1할 일 1할 일 1할 일 1", category: "TO_DO" },
-  { id: 2, title: "할 일 2", category: "DOING" },
-  { id: 3, title: "할 일 3", category: "DONE" },
-];
-
 export default function ToDoList() {
+  const [todos, setTodos] = useRecoilState(todoSelector);
+  const onAddTodo = (todo: ITodo) => {
+    localStorage.setItem("todos", JSON.stringify([...todos, todo]));
+    setTodos([...todos, todo]);
+  };
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos") || "[]");
+    setTodos(todos);
+  }, []);
   return (
-    <Container>
-      {ToDoItems.map((todo) => (
-        <Todo key={todo.id} todo={todo} />
-      ))}
-    </Container>
+    <>
+      <ToDoInput onAddTodo={onAddTodo} />
+      <Container>
+        {todos.map((todo) => (
+          <Todo key={todo.id} todo={todo} />
+        ))}
+      </Container>
+    </>
   );
 }
